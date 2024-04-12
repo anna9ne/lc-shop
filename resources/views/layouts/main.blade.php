e79d4072d50bbc89fad5a3e215e640baa9695d48
anna <anna9ne@gmail.com>
2024-04-12 21:21:46 +0300

    6. CRUD по тегам

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -158,7 +158,7 @@
                     </li>
 
                     <li class="nav-item">
-                        <a href="pages/gallery.html" class="nav-link">
+                        <a href="{{ route('tag.index') }}" class="nav-link">
                             <i class="nav-icon fas fa-tags"></i>
                             <p>Tags</p>
                         </a>
