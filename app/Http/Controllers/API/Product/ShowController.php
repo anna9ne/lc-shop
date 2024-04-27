0790b99d5e2d3976d0ae94fd37fa260ed3d72e1b
anna <anna9ne@gmail.com>
2024-04-27 13:52:07 +0300

    17. Выбор продукта внутри попап, улучшаем отдачу продуктов

--- app/Http/Controllers/API/Product/ShowController.php
+++ app/Http/Controllers/API/Product/ShowController.php
@@ -3,17 +3,15 @@
 namespace App\Http\Controllers\API\Product;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Product\IndexProductResource;
 use App\Http\Resources\Product\ProductResource;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
-class IndexController extends Controller
+class ShowController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Product $product)
     {
-        $products = Product::all();
-        return IndexProductResource::collection($products);
+        return new ProductResource($product);
     }
 }
