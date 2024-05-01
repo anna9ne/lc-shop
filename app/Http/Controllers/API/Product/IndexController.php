1bd90b491657311ac722c749805d98f2d42267c3
anna <anna9ne@gmail.com>
2024-05-01 20:03:29 +0300

    20. Фильтрация продуктов фронт + бекенд

--- app/Http/Controllers/API/Product/IndexController.php
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\API\Product;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\ProductFilter;
+use App\Http\Requests\API\Product\IndexRequest;
 use App\Http\Resources\Product\IndexProductResource;
 use App\Http\Resources\Product\ProductResource;
 use App\Models\Category;
@@ -11,9 +13,11 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function __invoke()
+    public function __invoke(IndexRequest $request)
     {
-        $products = Product::all();
+        $data = $request->validated();
+        $filter = app()->make(ProductFilter::class, ['queryParams' => array_filter($data)]);
+        $products = Product::filter($filter)->get();
         return IndexProductResource::collection($products);
     }
 }
